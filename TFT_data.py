a1455b81bc48b6b3bf231223a7dc5286bc16dcbb
Shayshu Nahata <55864616+Shayshu-NR@users.noreply.github.com>
2020-05-16 20:25:22 -0400

    Applied for twitter api, finsihed the majority of sql class and tft api class

--- TFT_data.py
+++ TFT_data.py
@@ -14,88 +14,119 @@ USER_NAME = 'DotsXL'
 #~~~~~~~~~~~~~~~~~~~~~~~~~
 
 #~~~~~Functions~~~~~
-#Establish connection with MySQL database
-def mysql_server_connect():
-    try:
-        connection = mysql.connector.connect(host='localhost',
-                             database='sql_match_history',
-                             user='shayshu',
-                             password='datalog')
+class mysql_database():
+    #Establish connection with MySQL database
+    def mysql_server_connect(self):
+        try:
+            connection = mysql.connector.connect(host='localhost',
+                                database ='sql_match_history',
+                                user ='shayshu',
+                                password ='datalog')
+            
+            if connection.is_connected():
+                print("Connected to database!")
+                return connection
+                
+        except mysql.connector.Error as error :
+            print("Failed to connect to database {}".format(error))
+            return False
 
-        return connection
-    except mysql.connector.Error as error :
-        print("Failed to connect to database {}".format(error))
+    #Close connection with MySQL database
+    def mysql_server_close(self, connection):
+        try:
+            connection.close()
+        except mysql.connector.Error as error :
+            print("Failed to close database connection {}".format(error))
+            return False
 
-#Close connection with MySQL database
-def mysql_server_close(connection):
-    try:
-        connection.close()
-    except mysql.connector.Error as error :
-        print("Failed to close database connection {}".format(error))
+    #Return the match history
+    def mysql_get_match_history(self, ID, connection):
+        try:
+            cursor  = connection.cursor()
+            sql_query = """select * from match_history"""
+            cursor.execute(sql_query, (ID))
+            records = cursor.fetchall()
 
-#Return the match history
-def mysql_get_match_history(ID):
+            #Print out each row from the database
+            for row in records:
+                print(row[0])
+                print(row[1])
+                print(row[2])
+
+            return records
+
+        except mysql.connector.Error as error :
+            print("Failed to read {}".format(error))
+            return False
+
+    #Check if a match ID is already in the database
+    def check_new_entry(self, match_id, connection):
+        try:
+            cursor  = connection.cursor()
+            sql_query = """select * from match_history where Match_History_ID = %s"""
+            cursor.execute(sql_query, (match_id))
+            records = cursor.fetchall()
+
+            #Print out each row from the database
+            for row in records:
+                print(row[0])
+                print(row[1])
+                print(row[2])
+
+            if records > 0 :
+                return False
+            else :
+                return True
+
+        except mysql.connector.Error as error :
+            print("Failed to read {}".format(error))
+            return False
     
-    try:
-        connection = mysql_server_connect()
-        cursor  = connection.cursor()
-        sql_query = """select * from match_history"""
-        cursor.execute(sql_query, (ID))
-        records = cursor.fetchall()
+    #Add a new entry to the mysql database
+    def new_entry(self, userName, puuid, match_id):
+        try:
+            connection = mysql.connector.connect(host='localhost',
+                                database='sql_match_history',
+                                user='shayshu',
+                                password='datalog')
+            cursor = connection.cursor()
+            sql_query = """insert into match_history (Summoner_Name, Puuid, Match_History_ID) VALUES(%s, %s, %s)"""
+            dataTuple = (userName, puuid, match_id)
+            cursor.execute(sql_query, dataTuple)
+            connection.commit()
 
-        #Print out each row from the database
-        for row in records:
-            print(row[0])
-            print(row[1])
-            print(row[2])
+            print("Added to database...")
+            return
 
-        return records
+        except mysql.connector.Error as error:
+            print("Failed to insert into MySQL table {}".format(error))
+            return False
 
-    except mysql.connector.Error as error :
-        print("Failed to read {}".format(error))
 
-def check_new_entry(match_id):
-    try:
-        connection = mysql_server_connect()
-        cursor  = connection.cursor()
-        sql_query = """select * from match_history where Match_History_ID = %s"""
-        cursor.execute(sql_query, (match_id))
-        records = cursor.fetchall()
+#TFT Data class
+class TFTData():
+    #Make a requrest to riot api and get the puuid from an:
+    #API key, and summoner name, 
+    def get_puuid(self, key, summonerName):
+        url_request = 'https://na1.api.riotgames.com/tft/summoner/v1/summoners/by-name/' +  summonerName + key
+        responseJson = requests.get(url_request).json()
+        puuid = responseJson['puuid']
 
-        #Print out each row from the database
-        for row in records:
-            print(row[0])
-            print(row[1])
-            print(row[2])
+        return puuid
 
-        return records
+    #Get the number of matches and their respective ids
+    def get_match_ids(self, key, puuid):
+        url_request = 'https://americas.api.riotgames.com/tft/match/v1/matches/by-puuid/' + puuid + '/ids' + key
+        responseJson = requests.get(url_request).json()
+        match_ids = responseJson
 
-    except mysql.connector.Error as error :
-        print("Failed to read {}".format(error))
+        return match_ids
 
-#Make a requrest to riot api and get the puuid from an:
-#API key, and summoner name, 
-def get_puuid(key, summonerName):
-    url_request = 'https://na1.api.riotgames.com/tft/summoner/v1/summoners/by-name/' +  summonerName + key
-    responseJson = requests.get(url_request).json()
-    puuid = responseJson['puuid']
+    #Get the match data for a given match id
+    def get_match_data(self, key, match_id):
+        url_request = 'https://americas.api.riotgames.com//tft/match/v1/matches/' + match_id + key
+        responseJson = requests.get(url_request).json()
 
-    return puuid
-
-#Get the number of matches and their respective ids
-def get_match_ids(key, puuid):
-    url_request = 'https://americas.api.riotgames.com/tft/match/v1/matches/by-puuid/' + puuid + '/ids' + key
-    responseJson = requests.get(url_request).json()
-    match_ids = responseJson
-
-    return match_ids
-
-#Get the match data for a given match id
-def get_match_data(key, match_id):
-    url_request = 'https://americas.api.riotgames.com//tft/match/v1/matches/' + match_id + key
-    responseJson = requests.get(url_request).json()
-
-    return responseJson
-    
+        return responseJson
 #~~~~~~~~~~~~~~~~~~~
 
--- main.py
+++ main.py
@@ -1,1 +1,29 @@
 #Runs the methods in order to generate results
+
+#~~~~~Libraries~~~~~
+from TFT_data import mysql_database
+from TFT_data import TFTData
+#~~~~~~~~~~~~~~~~~~~
+
+#~~~~~Global Variable~~~~~
+API_KEY = '?api_key=RGAPI-65c978e9-fc20-47df-9621-0a34c2826471'
+USER_NAME = 'DotsXL'
+#~~~~~~~~~~~~~~~~~~~~~~~~~
+
+#~~~~~Functions~~~~~
+def main():
+    database = mysql_database()
+    riotAPI = TFTData()
+
+    #Connect to mysql data base
+    database.mysql_server_connect()
+
+    #
+    puuid = riotAPI.get_puuid(API_KEY, USER_NAME)
+    match_ids = riotAPI.get_match_ids(API_KEY, puuid)
+    
+    for i in range(0, len(match_ids)):
+        print(i)
+#~~~~~~~~~~~~~~~~~~~
+
+main()
