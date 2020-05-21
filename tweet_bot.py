2ee40ef21d397c3f22a2e2b7da4433809ca60286
Shayshu Nahata <55864616+Shayshu-NR@users.noreply.github.com>
2020-05-20 20:20:06 -0400

    Mostly finished twitter bot

--- tweet_bot.py
+++ tweet_bot.py
@@ -5,4 +5,24 @@ import tweepy as tw
 import pandas as pd
 from twitter_keys import *
 #~~~~~~~~~~~~~~~~~~~~
+ 
+class Twitter_Bot():
+    def authorize(self, consumer, comsumer_secret):
+        auth = tw.OAuthHandler(consumer, consumer_secret)
 
+        return auth
+    
+    def access_toke(self, auth, token, token_secret):
+        auth.set_access_token(token, token_secret)
+
+        return auth
+    
+    def get_api(self, auth):
+        api = tw.API(auth, wait_on_rate_limit = True)
+
+        return api
+    
+    def update_status(self, api, status):
+        api.update_status(status)
+
+        return
