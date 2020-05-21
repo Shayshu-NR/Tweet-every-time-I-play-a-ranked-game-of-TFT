6128a1c93f6c303294d9d41ad3a2b041a78565df
Shayshu Nahata <55864616+Shayshu-NR@users.noreply.github.com>
2020-05-20 21:07:22 -0400

    Adding starting phrases

--- compose_tweet.py
+++ compose_tweet.py
@@ -4,17 +4,29 @@ import random
 
 #~~~~~Functions~~~~~
 def compose_tweet(level, placement, trait, damage):
-    starting_phrases_bad = ["Oh boy this was a rough one",
-                            "Welp another day another L",
-                            "Ah jeez Rick I'm pretty bad", 
-                            "LMAOOOOO GUESS WHO'S A CLOWN",
-                            "Press F in chat pls",
-                            ""
-                            ]
+    starting_phrases_bad = ["Oh boy this was a rough one, ",
+                            "Welp another day another L, ",
+                            "Ah jeez Rick I'm pretty bad, ", 
+                            "LMAOOOOO GUESS WHO'S A CLOWN??",
+                            "Press F in chat pls, ",
+                            "Ahaha fuuuuuuuuuuuuu, ",
+                            "Was I dropped on my head as a child? Because ",
+                            "Ayo turns out I'm trraaaaaashhh, ",
+                            "How do I unistall? ",
+                            "BRUUUHHHH ",
+                            "I just my 'yeet' myself, ya feel? ",
+                            "End my suffering, ",
+                            "I am stupid, ",
+                            "*Megolovania song* ",
+                            "My mom just called me a n00b cuz "]
 
-    starting_phrases_good = ["WOWZA what a pleasant surprise",
-                             "",
-                             ""
+    starting_phrases_good = ["WOWZA what a pleasant surprise, ",
+                             "It is I decent gamer2000 here, ",
+                             "*whips and nae-naes* ",
+                             "TSM SIGN ME ALREADY ",
+                             "Heck yea, ",
+                             "I AM LITTERYALLY GODDDDD, ",
+                             "Look at me now MOM, "
 
                             ]
     tweet = ""
